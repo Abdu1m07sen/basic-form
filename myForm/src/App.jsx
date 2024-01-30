d372417ebab29a956a8295fcd00d197496b190ac
Abdulmohsen Alnasser <142718274+Abdu1m07sen@users.noreply.github.com>
2024-01-30 09:40:38 +0300

    init

---
+++ myForm/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import FormComponent from './components/FormComponent'
+import { Container } from 'react-bootstrap'
+function App() {
+  return (
+    <Container>
+      <FormComponent/>
+    </Container>
+  )
+}
+
+export default App
